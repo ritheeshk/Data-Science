a4b6948b78864837c6d1f6a4074bc0cb89f742ec
ritheeshk <ritheeshkakita@gmail.com>
2018-07-14 15:02:09 +0530

    updated

--- Titanic/Scripts/Data_exploration.py
+++ Titanic/Scripts/Data_exploration.py
@@ -1,3 +1,16 @@
+#Import Packages
 import pandas as pd
 
+#Read the Data
 train=pd.read_csv('./Titanic/Data/train.csv')
+test=pd.read_csv('./Titanic/Data/test.csv')
+
+#Dataset Basic Details
+train.info()
+train.shape
+train.columns
+
+# Univariate analysis
+value_count=train.Sex.value_counts()
+value_count_target=train.groupby('Sex').Survived.sum()
+value_count_target.div(value_count)
